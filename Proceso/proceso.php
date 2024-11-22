18fc5a30181a506cb9122b23128254e713588bf6
Myngatte <zhouming2004@gmail.com>
2024-11-22 17:33:25 +0100

    procesoLogin actualizado

--- Proceso/proceso.php
+++ Proceso/proceso.php
@@ -7,19 +7,35 @@ if (isset($_POST['boton'])  && !empty($_POST['nombre']) && !empty($_POST['contra
     $_SESSION['usuario'] = $usuario;
     try {
         mysqli_autocommit($conexion, false);
-        $sql = "SELECT usuario_escuela, contra_usuario,rol_user FROM tbl_usuario WHERE usuario_escuela = ?";
+        $sql = "SELECT 
+        u.id_usuario, 
+        u.usuario_escuela, 
+        u.contra_usuario, 
+        r.nombre_rol
+        FROM 
+            tbl_usuario u
+        INNER JOIN 
+            tbl_rol r
+        ON 
+            u.rol_user = r.id_rol
+        WHERE u.usuario_escuela = ?";
         $stmt = mysqli_prepare($conexion, $sql);
         mysqli_stmt_bind_param($stmt, "s", $usuario);
         mysqli_stmt_execute($stmt);
         $resultado = mysqli_stmt_get_result($stmt);
 
         if ($usuario_db = mysqli_fetch_assoc($resultado)) {
+            $_SESSION['id_usuario'] = $usuario_db['id_usuario'];
+            if ($usuario_db['nombre_rol'] !== "admin") {
+                header('Location:../index.php?error=rol_invalido');
+                exit();
+            }else{
             if (password_verify($contra, $usuario_db['contra_usuario'])) {
-                $_SESSION['usuario'] = $usuario;
                 header("Location: ../menu.php");    
                 exit();
             } else {
                 header('Location:../index.php?error=contrasena_incorrecta');
+            }
             }
         } else {
             header('Location:../index.php?error=usuario_no_encontrado');
